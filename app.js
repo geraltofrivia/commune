e3fb6871895431d300ad16bf2f4f5865c534aa6f
Priyansh Trivedi <pc.priyansh@gmail.com>
2016-03-18 17:46:37 +0530

    String written in chatbox is passed key by key to console No filtering. No nothing. Merely set the connection.

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
-var express = require('express');
 var path = require('path');
+var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
 
 app.get('/', function (req, res) {
 	console.log('hellololo!');
-	// res.send('Hello World!');
-	res.sendFile(path.join(__dirname,'html/page.html'));
+	//Serving the main page
+	res.sendFile(path.join(__dirname,'/html/page.html'));
 });
 
-app.listen(3000, function() {
-	console.log('hello!');
+io.on('connection', function(socket){
+	console.log('A user connected!');
+	socket.on('disconnect', function() {
+		console.log('A user disconnected!');
+	});
+
+	socket.on('newmsg', function(msg) {
+		process.stdout.write(msg);
+	});
+});
+
+
+http.listen(3000, function() {
 	console.log('Example up and fucking running!');
 });
 
