feecb44d1c782180c52e31bb3d3cda590e5098ce
Priyansh Trivedi <pc.priyansh@gmail.com>
2016-03-18 14:38:21 +0530

    Static files served like a bitch

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 var express = require('express');
-var app = express()
+var path = require('path');
+var app = express();
 
 app.get('/', function (req, res) {
 	console.log('hellololo!');
-	res.send('Hello World!');
+	// res.send('Hello World!');
+	res.sendFile(path.join(__dirname,'html/page.html'));
 });
 
 app.listen(3000, function() {
 	console.log('hello!');
 	console.log('Example up and fucking running!');
 });
+
+//Serving static files
+app.use(express.static(path.join(__dirname,'public')));
